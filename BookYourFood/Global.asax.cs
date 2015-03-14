71c90d7a3242ffb7b3638268bd71513697f2865a
rstraszewski <rstraszewski@gmail.com>
2015-03-14 19:09:39 +0100

    Create database if not exist

--- BookYourFood/Global.asax.cs
+++ BookYourFood/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Database;
 
 namespace BookYourFood
 {
@@ -16,6 +17,11 @@ namespace BookYourFood
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            using (var byfDbContext = new ByfDbContext())
+            {
+                byfDbContext.Database.CreateIfNotExists();
+            }
         }
     }
 }
