30209e51e8b62ca737b9c5bcc13f3f500ec58b59
tomasz-grochmal <techtomgroch@gmail.com>
2015-05-24 18:53:44 +0200

    Add Table Creator

---
+++ ApplicationLayer/ReservationDomain/ReservationDomain.Service/TableService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Database;
+using ReservationDomain.Model;
+using Common.Service;
+
+namespace Reservaton.Service
+{
+    public class TableService : ApplicationService, ITableService
+    {
+        public TableService(ByfDbContext byfDbContext)
+            : base(byfDbContext)
+        {
+        }
+        public List<Table> GetTables()
+        {
+            return ByfDbContext.Tables.ToList();
+        }
+        public void CreateTable(Table table)
+        {
+            ByfDbContext.Tables.Add(table);
+            ByfDbContext.SaveChanges();
+        }
+
+        public void UpdateTable(Table table)
+        {
+            var tableEntity = ByfDbContext.Tables.Find(table.Id);
+            tableEntity.Reservations = table.Reservations;
+            tableEntity.SeatsNumber = table.SeatsNumber;
+            tableEntity.TableNumber = table.TableNumber;
+            ByfDbContext.SaveChanges();
+        }
+
+        public void RemoveTable(Table table)
+        {
+            var tableEntity = ByfDbContext.Tables.Find(table.Id);
+            ByfDbContext.Tables.Remove(tableEntity);
+            ByfDbContext.SaveChanges();
+        }
+    }
+}
---
+++ PresentationLayer/BookYourFood/Controllers/TableController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Reservaton.Service;
+using ReservationDomain.Model;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+
+namespace BookYourFood.Controllers
+{
+    public class TableController : Controller
+    {
+        private readonly ITableService tableService;
+
+        public TableController(ITableService tableService)
+        {
+            this.tableService = tableService;
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult GetTables([DataSourceRequest] DataSourceRequest request)
+        {
+            var tables = tableService.GetTables();
+            return Json(tables.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CreateTable([DataSourceRequest] DataSourceRequest request, Table table)
+        {
+            if (table != null && ModelState.IsValid)
+            {
+                tableService.CreateTable(table);
+            }
+            return Json(new[] { table }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult UpdateTable([DataSourceRequest] DataSourceRequest request, Table table)
+        {
+            if (table != null && ModelState.IsValid)
+            {
+                tableService.UpdateTable(table);
+            }
+
+            return Json(new[] { table }.ToDataSourceResult(request, ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult DestroyTable([DataSourceRequest] DataSourceRequest request, Table table)
+        {
+            if (table != null)
+            {
+                tableService.RemoveTable(table);
+            }
+
+            return Json(new[] { table }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
