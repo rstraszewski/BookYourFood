bbd0c8ee9629e5466462dae26a8bda592c201335
rstraszewski <rstraszewski@gmail.com>
2015-08-15 23:07:25 +0200

    Added untracked files.

---
+++ ApplicationLayer/ApplicationUserDomain/ApplicationUserBC.Configuratio/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationUserBC.Interfaces.DTOs;
+using ApplicationUserBC.Service;
+using ApplicationUserDomain.Infrastructure;
+using ApplicationUserDomain.Model.Repository;
+using ApplicationUserDomain.Service;
+using AutoMapper;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Identity.Model;
+using Microsoft.AspNet.Identity;
+
+namespace ApplicationUserBC.Configuration
+{
+    public class Startup : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            Mapper.CreateMap<UserAnswer, UserAnswerDto>();
+
+            container.Register(
+                Component.For<ApplicationUserDomainDbContext>()
+                    .LifestylePerWebRequest()
+                    .Named("ApplicationUserDomainDbContextDefault").IsDefault(),
+                Component.For<IApplicationUserService>()
+                    .ImplementedBy<ApplicationUserService>()
+                    .LifestyleTransient(),
+                Component.For<IApplicationUserRepository>()
+                    .ImplementedBy<ApplicationUserRepository>()
+                    .LifestyleTransient());
+        }
+        
+    }
+}
